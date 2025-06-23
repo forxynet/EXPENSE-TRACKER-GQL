25d59ed04e2bf09956f63d1c4d33cd7c023e5b97
forxynet <forxynet@gmail.com>
2025-06-23 13:34:31 +0300

    deployment setup complated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,10 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   // TOOD > update the uri on production
-  uri: "http://localhost:4000/graphql",
+  uri:
+    import.meta.env.VITE_NODE_ENV === "development"
+      ? "http://localhost:4000/graphql"
+      : "/graphql", //"http://localhost:4000/graphql",
   cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
   credentials: "include", // This tells Apollo Client to send along with every request to the server
 });
