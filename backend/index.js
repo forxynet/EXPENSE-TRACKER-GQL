eaa4a2d0969bc38f0f26db8fbabddf6d31348719
forxynet <forxynet@gmail.com>
2025-06-23 13:58:25 +0300

    updated index

--- backend/index.js
+++ backend/index.js
@@ -70,7 +70,7 @@ await server.start();
 app.use(
   "/graphql",
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:4000",
     credentials: true,
   }),
   express.json(),
